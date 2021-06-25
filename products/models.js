7415a68b811e3ae99e2827a305f2c85efc7ffa21
Suliman Tekalli <sulimantekalli@gmail.com>
2021-06-25 16:26:24 -0400

    Test connections between models and endpoints.

--- products/models.js
+++ products/models.js
@@ -1,14 +1,14 @@
 const db = require('./db/db.js')
 
 
-/**********************
+/***************************************
  *
- * QUERIES
+ *                  QUERIES
  *
- *********************/
+ ***************************************/
 
 
-let getProducts = () => {
+let queryProducts = () => {
   return new Promise((resolve, reject) => {
     // let q = `SELECT * FROM Products`
     // db.query(q, (err, result)=> {
@@ -16,13 +16,14 @@ let getProducts = () => {
       if (err) {
         reject(new Error(err))
       } else {
-        resolve(result)
+        resolve('it works at the moment')
+        // resolve(result)
       }
-    // })
-  })
-}
+      // })
+    })
+  }
 
-let getProductById = (productID) => {
+let queryProductById = (productID) => {
   return new Promise((resolve, reject) => {
     // let q = `SELECT * FROM Products WHERE ID = ${productID};`
     // db.query(q, (err, result)=> {
@@ -30,13 +31,14 @@ let getProductById = (productID) => {
       if (err) {
         reject(new Error(err))
       } else {
-        resolve(result)
-      }
+      resolve('it works at the moment')
+      // resolve(result)
+    }
     // })
   })
 }
 
-let getProductStyles = (productID) => {
+let queryProductStyles = (productID) => {
   return new Promise((resolve, reject) => {
     // let q = `SELECT * FROM Product_Styles WHERE Product_ID = ${productID};`
     // db.query(q, (err, result)=> {
@@ -44,14 +46,15 @@ let getProductStyles = (productID) => {
       if (err) {
         reject(new Error(err))
       } else {
-        resolve(result)
+        resolve('it works at the moment')
+        // resolve(result)
       }
-    // })
-  })
-}
+      // })
+    })
+  }
 
 
-let getFeatures = (productID) => {
+let queryFeatures = (productID) => {
   return new Promise((resolve, reject) => {
     // let q = `SELECT * FROM Features WHERE Product_ID = ${productID};`
     // db.query(q, (err, result)=> {
@@ -59,13 +62,14 @@ let getFeatures = (productID) => {
       if (err) {
         reject(new Error(err))
       } else {
-        resolve(result)
+        resolve('it works at the moment')
+        // resolve(result)
       }
-    // })
-  })
-}
+      // })
+    })
+  }
 
-let getSKUs = (styleID) => {
+let querySKUs = (styleID) => {
   return new Promise((resolve, reject) => {
     //THIS MAY HAVE TO BE A JOINED TABLE
     // let q = `SELECT * FROM SKUs WHERE Style_ID = ${styleID};`
@@ -74,13 +78,14 @@ let getSKUs = (styleID) => {
       if (err) {
         reject(new Error(err))
       } else {
-        resolve(result)
+        resolve('it works at the moment')
+        // resolve(result)
       }
-    // })
-  })
+      // })
+    })
 }
 
-let getPhotos = (styleID) => {
+let queryPhotos = (styleID) => {
   return new Promise((resolve, reject) => {
     // let q = `SELECT * FROM Photos WHERE Style_ID = ${styleID}`
     // db.query(q, (err, result)=> {
@@ -88,148 +93,153 @@ let getPhotos = (styleID) => {
       if (err) {
         reject(new Error(err))
       } else {
-        resolve(result)
+        resolve('it works at the moment')
+        // resolve(result)
       }
     })
-  // })
+    // })
 }
 
-let getRelatedProducts = (productID) => {
+let queryRelatedProducts = (productID) => {
   return new Promise((resolve, reject) => {
-    // let q = `SELECT * FROM Related_Products WHERE ID = ${productID}`
-    // db.query(q, (err, result)=> {
-      let err = undefined //DELETE THIS
-      if (err) {
-        reject(new Error(err))
-      } else {
-        resolve(result)
-      }
-    })
+  // let q = `SELECT * FROM Related_Products WHERE ID = ${productID}`
+  // db.query(q, (err, result)=> {
+    let err = undefined //DELETE THIS
+    if (err) {
+      reject(new Error(err))
+    } else {
+      resolve('it works at the moment')
+      // resolve(result)
+    }
+  })
   // })
 }
 
-/**********************
+/************************************
  *
- * ENDPOINT ROUTINGS
+ *            ENDPOINT ROUTINGS
  *
- *********************/
+ ************************************/
 
 
 
-const getAllProductsEndpoint = () => {
+const getAllProducts = () => {
   //query products table
-  getProducts()
-  .then(allProducts => {
-    let formattedData = 'TODO:';
-    console.log('format this', allProducts);
-    resolve(formattedData);
-  })
-  .catch(err => {
-    error = new Error(error)
-    console.log(error)
-    reject(error)
-  })
-}
-
-const getProductByIdEndpoint = (productID) => {
-  //query products table simultaneously below
-  let productPromise = getProductById(productID)
-  let featuresPromise = getFeatures(productID);
-  return Promise.all([productPromise, featuresPromise])
-    .then(allData => {
-      let product = allData[0]
-      let features = allData[1]
-
-      console.log('format product: ', product);
-      console.log('format features: ', features);
-
+  return new Promise ((resolve, reject) => {
+    queryProducts()
+    .then(allProducts => {
       let formattedData = 'TODO:';
-      //combine  and format returned data
-      // formattedData = {
-      //   id: TODO:,
-      //   name: TODO:,
-      //   slogan: TODO:,
-      //   description: TODO:,
-      //   category: TODO:,
-      //   default_price: TODO:,
-      //   features: []
-      // }
-      // //iterate through features
-      // //for each feature
-      // let feature = {
-      //   feature: TODO:,
-      //   value: TODO:
-      // }
-      // done with iteration
+      console.log('format this', allProducts);
       resolve(formattedData);
-   })
-   .catch(error=> {
-    error = new Error(error)
-    console.log(error)
-    reject(error)
-   })
-}
-
-const getProductStylesEndpoint = (productID) => {
-  //query product_styles table
-  getProductStyles(productID);
-  .then(results => {
-    //after query this query is done RESULT SHOULD BE MULTIPLE ROWS
-    let styleID;
-    let styles; //this will be an array of all style row
-    let skusPromises = [];
-    let photosPromises = [];
-    //iterate through all rows
-    for (let i = 0; i < styles.length; i++) {
-      styleID //will equal the current iterated rows id
-      skusPromises.push(getSKUs(styleID));
-      photosPromises.push(getPhotos(styleID));
-    }
-    let allDataPromises = [skusPromises, photosPromises]
-    return Promise.all(allDataPromises)
-  })
-  .then(allData => {
-    let skus = allData[0]
-    let photos = allData[1]
-
-    //STYLES TEMPLATE
-    let formattedData = {
-      product_id: `${productID}`,
-      results: [] //this will be filled with objects of nested data
-    }
-
-    let styleResult = {
-      style_id: null,
-      name: null,
-      original_price: null,
-      sale_price: null,
-      'default?': null,
-      photos: null, //this needs to be an array
-      skus: null//this will be an object of objects
-    }
-
-    let photos = {
-      thumbnail_url: null,
-      url: null
-    }
-
-    let skus = {}
-    for (let i = 0; i < styles.length; i++) {
-      //FILL OUT TEMPLATE HERE;
-    }
-    resolve(formattedData);
-  })
-  .catch(error => {
-    error = new Error(error)
-    console.log(error)
-    reject(error)
+    })
+    .catch(error => {
+      error = new Error(error)
+      console.log(error)
+      reject(error)
+    })
   })
 }
 
-const getRelatedProductsEndpoint = (productID) => {
+const getProductById = (productID) => {
+  return new Promise ((resolve, reject) => {
+    let productPromise = queryProductById(productID)
+    let featuresPromise = queryFeatures(productID);
+    return Promise.all([productPromise, featuresPromise])
+      .then(allData => {
+        let product = allData[0]
+        let features = allData[1]
+
+        console.log('format product: ', product);
+        console.log('format features: ', features);
+
+        let formattedData = 'TODO:';
+        //combine  and format returned data
+        // formattedData = {
+        //   id: TODO:,
+        //   name: TODO:,
+        //   slogan: TODO:,
+        //   description: TODO:,
+        //   category: TODO:,
+        //   default_price: TODO:,
+        //   features: []
+        // }
+        // //iterate through features
+        // //for each feature
+        // let feature = {
+        //   feature: TODO:,
+        //   value: TODO:
+        // }
+        // done with iteration
+        resolve(formattedData);
+     })
+     .catch(error=> {
+      error = new Error(error)
+      console.log(error)
+      reject(error)
+     })
+  })
+}
+
+const getProductStyles = (productID) => {
+  return new Promise((resolve, reject) => {
+    queryProductStyles(productID)
+    .then(results => {
+      //after query this query is done RESULT SHOULD BE MULTIPLE ROWS
+      let styleID;
+      let styles; //this will be an array of all style row
+      let skusPromises = [];
+      let photosPromises = [];
+      //iterate through all rows
+      for (let i = 0; i < styles.length; i++) {
+        styleID //will equal the current iterated rows id
+        skusPromises.push(querySKUs(styleID));
+        photosPromises.push(queryPhotos(styleID));
+      }
+      let allDataPromises = [skusPromises, photosPromises]
+      return Promise.all(allDataPromises)
+    })
+    .then(allData => {
+      let skusData = allData[0]
+      let photosData = allData[1]
+
+      //STYLES TEMPLATE
+      let formattedData = {
+        product_id: `${productID}`,
+        results: [] //this will be filled with objects of nested data
+      }
+
+      let styleResult = {
+        style_id: null,
+        name: null,
+        original_price: null,
+        sale_price: null,
+        'default?': null,
+        photos: null, //this needs to be an array
+        skus: null//this will be an object of objects
+      }
+
+      let photos = {
+        thumbnail_url: null,
+        url: null
+      }
+
+      let skus = {}
+      for (let i = 0; i < styles.length; i++) {
+        //FILL OUT TEMPLATE HERE;
+      }
+      resolve(formattedData);
+    })
+    .catch(error => {
+      error = new Error(error)
+      console.log(error)
+      reject(error)
+    })
+  })
+}
+
+const getRelatedProducts = (productID) => {
   return new Promise ((resolve, reject) =>{
-    //query related table
-    getRelatedProducts()
+    queryRelatedProducts()
     .then(results => {
       //check format
       //probably JSON.parse(results?)
@@ -246,8 +256,8 @@ const getRelatedProductsEndpoint = (productID) => {
 }
 
 module.exports = {
-  getAllProductsEndpoint: getAllProductsEndpoint,
-  getProductByIdEndpoint: getProductByIdEndpoint,
-  getProductStylesEndpoint: getProductStylesEndpoint
-  getRelatedProductsEndpoint: getRelatedProductsEndpoint
+  getAllProducts: getAllProducts,
+  getProductById: getProductById,
+  getProductStyles: getProductStyles,
+  getRelatedProducts: getRelatedProducts
 }
--- products/server.js
+++ products/server.js
@@ -1,32 +1,62 @@
 const express = require('express')
+const models = require('./models.js');
 const port = 3000
 const app = express();
+app.use(express.json())
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 })
-app.use(express.json())
 
-///products
+
+/**********************************
+ *
+ *             ROUTES
+ *
+ *********************************/
 
 app.get('/products', (req, res) => {
-  res.sendStatus(200)
+  models.getAllProducts()
+  .then(result => {
+    console.log(result)//EXPECT result to be 'TODO:'
+    res.sendStatus(200)
+  })
 })
-//
-///produgcts/<productID>
+
+
 app.get('/products/:product_id', (req, res) => {
-  console.log(req.params)
-
-  res.sendStatus(200)
+  // console.log(req.params)
+  models.getProductById(10011)
+  .then(result => {
+    console.log(result)//EXPECT result to be 'TODO:'
+    res.sendStatus(200)
+  })
+  .catch(error => {
+    console.log(new Error(error))
+  })
 })
-///products/<productID>/styles
+
+
 app.get('/products/:product_id/styles', (req, res) => {
-  console.log(req.params)
-
-  res.sendStatus(200)
+  // console.log(req.params)
+  models.getProductStyles(10011)
+  .then(result => {
+    console.log(result)//EXPECT result to be {product_id: 'something', results: []}
+    res.sendStatus(200)
+  })
+  .catch(error => {
+    console.log(new Error(error))
+  })
 })
-///related
-app.get('/products/:product_id/related', (req, res) => {
-  console.log(req.params)
 
-  res.sendStatus(200)
+
+app.get('/products/:product_id/related', (req, res) => {
+  // console.log(req.params)
+  models.getRelatedProducts(10011)
+  .then(result => {
+    console.log(result)//EXPECT result to be ['TODO:']
+    res.sendStatus(200)
+  })
+  .catch(error => {
+    console.log(new Error(error))
+  })
 })
